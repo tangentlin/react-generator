fb59b53366ea4c7d3f70ea1f582985a58e352b77
Tangent Lin <tangent@usa.net>
2019-07-14 17:12:01 -0400

    More scaffolding files

---
+++ tools/build.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { ncp } = require('ncp');
+const rimraf = require('rimraf');
+
+function build() {
+  clean();
+  copySource( () => adjustPackageJson() );
+}
+
+function clean() {
+  rimraf.sync('dist');
+}
+
+function copySource(next) {
+  ncp('src', 'dist', (err) => {
+    if (next) {
+      next();
+    }
+  });
+}
+
+function adjustPackageJson() {
+  const rawData = fs.readFileSync('package.json');
+  const content = JSON.parse(rawData);
+
+  content.schematics = './collection.json';
+  const newJson = JSON.stringify(content, null, 2);
+  fs.writeFilesync('dist/package.json', newJson);
+}
